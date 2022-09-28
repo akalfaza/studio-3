b5970eed336aab6b68c6b452fe71f6f95ebc3210
akalfaza <alara_kalfazade@brown.edu>
2022-09-28 11:56:03 -0400

    studio 3

--- app.js
+++ app.js
@@ -26,14 +26,32 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  //console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
+  const fruitsSugar = fruitsArray.filter(fruit=>{
+    return fruit.nutritions.sugar > 15;
+  })
+
+  const fruitNames = fruitsSugar.map(fruit =>{
+    return fruit.name;
+  })
+
+  console.log(fruitNames)
+
+
   // TODO: Create a new HTML element to display your data
+  const addElement = () => {
+    const element = document.createElement('div');
+    element.innerHTML = fruitNames;
+    const element2 = document.getElementById('cs1300-gallery');
+    element2.append(element);
+  }
 
   // TODO: Append your new element to the page
+  addElement()
 
 }
 
